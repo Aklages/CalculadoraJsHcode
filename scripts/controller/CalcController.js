659a2be14833422209301226df6c80820305829a
André Lages <99544874+Aklages@users.noreply.github.com>
2024-05-07 19:53:59 -0300

    Add files via upload

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -84,13 +84,20 @@ class CalcController{
 
         let last = '';
 
-        if(this._operation.length > 3){
-            last = this._operation.pop();
+        this._lastOperator = this.getLastItem();
 
-            let result = this.getResult();
-
+        if(this._operation.length < 3){
+            let firstItem = this._operation[0]; 
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
         }
 
+        if(this._operation.length > 3){
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        }else if(this._operation.length = 3){
+            this._lastNumber = this.getLastItem(false);
+        }
 
         let result = this.getResult();
 
@@ -110,18 +117,29 @@ class CalcController{
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay(){
+    getLastItem(isOperator = true){
 
-        let lastNumber;
+        let lastItem;
 
         for(let i = this._operation.length-1; i >= 0; i--){
 
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
                 break;
+                }
             }
 
+            if (!lastItem){
+                lastItem = (isOperator) ? this._lastOperator : this.lastNumber;
+            }
+
+            return lastItem;
+
         }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber = this.getLastItem(false);
 
         if(!lastNumber) lastNumber = 0;
 
